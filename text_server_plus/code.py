cd5e9061d4d89e926624059f4cf6dce5d3039f51
Neradoc <neraOnGit@ri1.fr>
2023-04-13 17:06:29 +0200

    remove text_server sample text_server_plus reorganized - remove mdns code - remove external display setup - add size field in the form and use it for the display

--- text_server_plus/code.py
+++ text_server_plus/code.py
@@ -16,35 +16,21 @@ from adafruit_httpserver.response import HTTPResponse
 from adafruit_httpserver.mime_type import MIMEType
 from adafruit_httpserver.status import CommonHTTPStatus
 
-MDNS_NAME = "text-setter"
-PORT = 8080
+PORT = 8000
 ROOT = "/www"
-
-# setup I2C display
-I2C_ADDRESS = 0x3D
-DISPLAY_WIDTH = 128
-DISPLAY_HEIGHT = 64
-
-SCALE = 1
 
 ############################################################################
 # Display
 ############################################################################
 
 import displayio
+SCALE = 1
 
 if hasattr(board, "DISPLAY"):
     display = board.DISPLAY
 else:
-    from adafruit_displayio_ssd1306 import SSD1306
-    displayio.release_displays()
-    try:
-        i2c = board.I2C()
-    except RuntimeError:
-        i2c = board.STEMMA_I2C()
-
-    display_bus = displayio.I2CDisplay(i2c, device_address=I2C_ADDRESS)
-    display = SSD1306(display_bus, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT)
+    # setup an external display in the display variable.
+    raise OSError("Please setup an external display for this demo")
 
 ############################################################################
 # Interface on the display
@@ -57,7 +43,6 @@ from adafruit_display_text import wrap_text_to_lines
 FONT = terminalio.FONT
 
 box = FONT.get_bounding_box()
-LINE_WRAP = display.width // (box[0] * SCALE)
 
 color_bitmap = displayio.Bitmap(display.width, display.height, 1)
 color_palette = displayio.Palette(1)
@@ -76,14 +61,11 @@ text_area = Label(
 
 splash.append(text_area)
 display.show(splash)
+display.auto_refresh = False
 
-############################################################################
-# MDNS
-############################################################################
-
-mdnserv =  mdns.Server(wifi.radio)
-mdnserv.hostname = MDNS_NAME
-mdnserv.advertise_service(service_type="_http", protocol="_tcp", port=PORT)
+def wrap_the_text(text):
+    LINE_WRAP = display.width // (box[0] * text_area.scale)
+    return wrap_text_to_lines(text, LINE_WRAP)
 
 ############################################################################
 # wifi
@@ -104,19 +86,37 @@ server = HTTPServer(pool)
 
 ERROR400 = CommonHTTPStatus.BAD_REQUEST_400
 
-@server.route("/text", method="POST")
+@server.route("/receive", method="POST")
 def base(request):
-    # receive a text in the body
-    body = json.loads(request.body)
     try:
-        the_text = body.get("the_text", "")
-        message = "\n".join(wrap_text_to_lines(the_text, LINE_WRAP))
+        # receive a text in the body
+        body = json.loads(request.body)
+        ########################################################
+        # extract the size field
+        size = body.get("size", None)
+        # change the scale
+        if size:
+            try:
+                text_area.scale = size
+            except ValueError:
+                print("Size invalid")
+        ########################################################
+        # extract the text field
+        the_text = body.get("text", "")
+        # prepare the message for the screen
+        message = "\n".join(wrap_the_text(the_text))
+        # show the message
         text_area.text = message
         print(f"Received:", message)
+        ########################################################
+        # refresh the display after all the changes
+        display.refresh()
+        # respond ok
         with HTTPResponse(request) as response:
             response.send("ok")
     except (ValueError, AttributeError) as err:
-        traceback.print_exception(err, err, err.__traceback__)
+        # show the error if something went wrong
+        traceback.print_exception(err)
         with HTTPResponse(request, status=ERROR400) as response:
             response.send("error")
 
